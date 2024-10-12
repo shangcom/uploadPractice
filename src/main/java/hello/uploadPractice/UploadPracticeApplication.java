b90b52749d73175e784e67c28c78607667ed79a5
shangcom <dndbs91@gmail.com>
2024-10-12 20:18:48 +0900

    Initial commit

---
+++ src/main/java/hello/uploadPractice/UploadPracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.uploadPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/uploadPractice/UploadPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.uploadPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
